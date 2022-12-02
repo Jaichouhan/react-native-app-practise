19eb13539ca4e39897f488680b2830e61cf7f205
Jai chouhan <92376924+Jaichouhan@users.noreply.github.com>
2022-12-02 18:08:57 +0530

    one component created done

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,41 @@
 import React from "react";
-import { Text } from "react-native";
-
+import { Text, SafeArea, FlatList, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS } from "../constants";
+import FocusStatusBar from "../components/FocusStatusBar";
+import HomeHeader from "../components/HomeHeader";
+import NFTCard from "../components/NFTCard";
+import NFTData from "../constants/dummy";
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
